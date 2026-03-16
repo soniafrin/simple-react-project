2a6c1c04e39abfdbee5273120559693da4eddf4f
soniafrin <soniafrin50@gmail.com>
2026-03-16 12:44:07 +0000

    fetching all countries

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
+import Country from './Country'
+
+const fetchCountries = async() =>{
+  const res = await fetch('https://openapi.programming-hero.com/api/all')
+  const data = res.json()
+  return data
+}
+console.log(fetchCountries())
+const countryPromise = fetchCountries()
+console.log(countryPromise)
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Country></Country>
     </>
   )
 }
